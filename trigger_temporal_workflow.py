1580c82a70dea23aa1a6067046c80de86f0d50e0
Umer Arshad <umerm64@gmail.com>
2023-10-19 16:52:09 +0500

    patch

--- trigger_temporal_workflow.py
+++ trigger_temporal_workflow.py
@@ -56,7 +56,8 @@ response = requests.post(
 
 if response.status_code == 201:
     print(f'Pull request created successfully: {response.json()["html_url"]}')
-    print(response.json()['id'])
+    print("repo id:", str(response.json()['head']['repo']['id']))
+    print("pr id:", response.json()['id'])
     # time.sleep(5)
     res = requests.post(url="https://api.breu.ngrok.io/providers/github/artifact-ready", data=json.dumps({
             "pull_request_id": str(response.json()['id']),
